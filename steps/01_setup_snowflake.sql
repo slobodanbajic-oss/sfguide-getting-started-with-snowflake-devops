146263f8953d335503e9ff743d622c1b692f9437
Slobodan Bajic <slobodan.bajic@vegaitglobal.com>
2025-12-12 14:31:25 +0100

    local_update

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -7,23 +7,23 @@ CREATE OR ALTER WAREHOUSE QUICKSTART_WH
 
 
 -- Separate database for git repository
-CREATE OR ALTER DATABASE QUICKSTART_COMMON;
+CREATE OR REPLACE DATABASE QUICKSTART_COMMON;
 
 
 -- API integration is needed for GitHub integration
 CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('https://github.com/<insert GitHub username>') -- INSERT YOUR GITHUB USERNAME HERE
+  API_ALLOWED_PREFIXES = ('https://github.com/slobodanbajic-oss') -- INSERT YOUR GITHUB USERNAME HERE
   ENABLED = TRUE;
 
 
 -- Git repository object is similar to external stage
 CREATE OR REPLACE GIT REPOSITORY quickstart_common.public.quickstart_repo
   API_INTEGRATION = git_api_integration
-  ORIGIN = '<insert URL of forked GitHub repo>'; -- INSERT URL OF FORKED REPO HERE
+  ORIGIN = 'https://github.com/slobodanbajic-oss/sfguide-getting-started-with-snowflake-devops'; -- INSERT URL OF FORKED REPO HERE
 
 
-CREATE OR ALTER DATABASE QUICKSTART_PROD;
+CREATE OR REPLACE DATABASE QUICKSTART_PROD;
 
 
 -- To monitor data pipeline's completion
@@ -33,14 +33,14 @@ CREATE OR REPLACE NOTIFICATION INTEGRATION email_integration
 
 
 -- Database level objects
-CREATE OR ALTER SCHEMA bronze;
-CREATE OR ALTER SCHEMA silver;
-CREATE OR ALTER SCHEMA gold;
+CREATE OR REPLACE SCHEMA bronze;
+CREATE OR REPLACE SCHEMA silver;
+CREATE OR REPLACE SCHEMA gold;
 
 
 -- Schema level objects
 CREATE OR REPLACE FILE FORMAT bronze.json_format TYPE = 'json';
-CREATE OR ALTER STAGE bronze.raw;
+CREATE OR REPLACE STAGE bronze.raw;
 
 
 -- Copy file from GitHub to internal stage
